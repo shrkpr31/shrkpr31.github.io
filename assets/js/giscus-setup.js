68675cea48d5e62e4f91f62dd8c0c71cd93e808e
shrkpr31 <shr.kpr1995@gmail.com>
2025-12-22 07:52:17 +0000

    Deploying to gh-pages from @ shrkpr31/shrkpr31.github.io@52e14436ba95c0013bef5b4457fb0d2aa219a894 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "shrkpr31/shreyakapoor.github.io",
+    "data-repo": "shrkpr31/shrkpr31.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
