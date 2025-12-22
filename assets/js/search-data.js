4485ba610b55535383192e665cd9968b3ea767bd
shrkpr31 <shr.kpr1995@gmail.com>
2025-12-22 09:59:16 +0000

    Deploying to gh-pages from @ shrkpr31/shrkpr31.github.io@4fe343cadee455c5758db1c92660ecd63222d541 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,19 +16,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "nav-projects",
           title: "projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
+          },
+        },{id: "nav-cv",
+          title: "cv",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
           title: "teaching",
