58737c583a64462ef6e98909c4e71fa3f7410454
shrkpr31 <shr.kpr1995@gmail.com>
2025-12-24 07:48:33 +0000

    Deploying to gh-pages from @ shrkpr31/shrkpr31.github.io@370662cf63f8669a8a29db6d1fe1c317a5258795 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -409,8 +409,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-has-bhoo-chetna-rejuvenated-agriculture-in-karnataka-an-impact-assessment",
+          title: 'Has Bhoo-chetna rejuvenated agriculture in Karnataka? An impact assessment',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
